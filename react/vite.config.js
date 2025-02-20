4fcfa97d65b6aa35a2d6d577c9bff52a9183baaf
subhajit das <subhajitdas103@gmail.com>
2025-02-20 13:26:01 +0530

    up

--- react/vite.config.js
+++ react/vite.config.js
@@ -7,17 +7,35 @@
 // })
 
 // =================
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import fs from 'fs'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     https: {
+//       key: fs.readFileSync('localhost-key.pem'),
+//       cert: fs.readFileSync('localhost.pem'),
+//     },
+//     host: 'localhost',
+//     port: 5173,
+//   },
+// })
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import fs from 'fs';
+import path from 'path';
 
 export default defineConfig({
+  plugins: [react()],
   server: {
     https: {
-      key: fs.readFileSync('localhost-key.pem'),
-      cert: fs.readFileSync('localhost.pem'),
-    }
+      key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem')),
+    },
+    host: 'localhost',
+    port: 5173,
   },
-  plugins: [react()],
 });
-
