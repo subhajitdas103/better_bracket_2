3e3fdfca4c3e49bc45b9e9d2cdde39f94de8a3a3
subhajit das <subhajitdas103@gmail.com>
2025-02-27 11:04:14 +0530

    up

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,7 +1,7 @@
 <?php
 use App\Http\Controllers\Auth\GoogleAuthController;
-<<<<<<< HEAD
 use App\Http\Controllers\Auth\FacebookLoginController;
+use Illuminate\Support\Facades\Route;
 // Route::post('google-login', [GoogleAuthController::class, 'handleGoogleLogin']);
 // Route::post('google-login', [GoogleAuthController::class, 'handleGoogleLogin'])->withoutMiddleware('auth:sanctum');
 Route::post('google-login', [GoogleAuthController::class, 'handleGoogleLogin'])
@@ -15,12 +15,10 @@ Route::post('google-login', [GoogleAuthController::class, 'handleGoogleLogin'])
     ->withoutMiddleware('auth:sanctum');
 
 
-    
-=======
-use Illuminate\Support\Facades\Route;
+
   // Make sure to use the correct controller
 
 // Route::post('google-login', [AuthController::class, 'handleGoogleLogin']);
 Route::post('/google-login', [GoogleAuthController::class, 'handleGoogleLogin']);
 // Route::get('/Students',[StudentController::class,'fetchStudentData']);
->>>>>>> parent of 1a36418 (up)
+
