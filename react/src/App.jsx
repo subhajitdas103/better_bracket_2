9e8aaff5b9a618d1e21c4a9007f81a3ba27845ee
Ab6720 <analbera211@gmail.com>
2025-03-26 11:19:16 +0530

    sidebar

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,25 +1,30 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link, Navigate } from "react-router-dom";
 import Login from "./Pages/Login";
 import Signup from "./Pages/Signup";
 import ForgotPassword from "./Pages/ForgotPassword";
 import Dashboard from "./Pages/Dashboard";
-import logo from "./assets/nn.png"; // 
-// import bg from "../assets/soccer-players-action-professional-stadium.jpg";
-import { GoogleLogin } from '@react-oauth/google';
-const App = () => {
+import Sidebar from "./Components/Sidebar";
+import logo from "./assets/nn.png";
 
+// Layout Wrapper for Pages with Sidebar
+const DashboardLayout = ({ children }) => (
+  <div className="flex">
+    <Sidebar />
+    <div className="flex-1 p-4">{children}</div>
+  </div>
+);
+
+const App = () => {
   return (
     <Router>
       <div>
+        {/* Navbar */}
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <div className="container-fluid">
-
-
-<Link className="navbar-brand" to="/">
-  <img src={logo} alt="Better Bracket" width="100" height="100" style={{ marginTop: "-33px" }} />
-</Link>
-
+            <Link className="navbar-brand" to="/">
+              <img src={logo} alt="Better Bracket" width="100" height="100" style={{ marginTop: "-33px" }} />
+            </Link>
             <button
               className="navbar-toggler"
               type="button"
@@ -32,7 +37,7 @@ const App = () => {
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav ms-auto" style={{marginTop:"-33px"}}>
+              <ul className="navbar-nav ms-auto" style={{ marginTop: "-33px" }}>
                 <li className="nav-item">
                   <Link className="nav-link" to="/login">Login</Link>
                 </li>
@@ -44,17 +49,19 @@ const App = () => {
           </div>
         </nav>
 
+        {/* Routes */}
         <div className="container mt-4">
           <Routes>
-          <Route path="/" element={<Signup />} /> {/* Default route */}
+            <Route path="/" element={<Navigate to="/login" />} /> {/* Redirect default route */}
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
-            <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/forgot-password" element={<ForgotPassword />} />
+
+            {/* Dashboard with Sidebar */}
+            <Route path="/dashboard" element={<DashboardLayout><Dashboard /></DashboardLayout>} />
           </Routes>
         </div>
       </div>
-   
     </Router>
   );
 };
--- react/src/Pages/Dashboard.jsx
+++ react/src/Pages/Dashboard.jsx
@@ -1,15 +1,16 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
+import Sidebar from "../Components/Sidebar";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Sidebar from "../components/sidebar";
-const Dashboard = () => {
 
+const Dashboard = () => {
   return (
-    <div className="container">
-      <div className="row justify-content-center">
-        <div className="col-md-4">
-          <h2 className="text-center mb-4">Welcome to Dashboard</h2>
-        </div>
+    <div className="d-flex">
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* Main Content */}
+      <div className="flex-grow-1 p-4">
+        <h2 className="text-center mb-4">Welcome to Dashboard</h2>
       </div>
     </div>
   );
